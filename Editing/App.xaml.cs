1d34fb4f03c82cc5cdc8419a948730ddde509722
SheheryarSherry <sheheryarkhan1992@hotmail.com>
2018-08-07 12:32:27 +0500

    Export success view added

--- Editing/App.xaml.cs
+++ Editing/App.xaml.cs
@@ -72,7 +72,7 @@ namespace Editing
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(ExportProgress), e.Arguments);
+                    rootFrame.Navigate(typeof(ExportSuccessfull), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
